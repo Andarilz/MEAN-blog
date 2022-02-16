af7e7c4e3fead6b27e1479b6ac2cc21aaa0cf11d
Andarilz <tartarus@gmail.com>
2022-02-16 06:17:50 +0300

    router

--- index.js
+++ index.js
@@ -4,15 +4,34 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
+const config = require('./config/db') //путь к инфе о ссылке к БД
+const account = require('./routes/account')
 
 const app = express();
 
 const port = 3000;
 
-app.listen(port, () => {
-    console.log("Сервер работает на порту: " + port)
-});
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use('/account', account)
+
+mongoose.connect(config.db, {useNewUrlParser: true, useUnifiedTopology: true}) //подключаемся к БД
+
+mongoose.connection.on('connected', () => {
+    console.log('Успешный коннект к БД')
+})
+
+mongoose.connection.on('error', er => {
+    console.log('Ошибка подключения к бд', er)
+})
 
 app.get('/', (req, res) => {
     res.send("Главная страница ")
+});
+
+
+app.listen(port, () => {
+    console.log("Сервер работает на порту: " + port)
 });
